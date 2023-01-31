46baa6969e0a8521d49419636a4cb398ead66fa2
Tim Sullivan <timothyj.sullivan1@gmail.com>
2023-01-31 15:30:47 -0600

    updated providers to include managed environment

--- infra/main.tf
+++ infra/main.tf
@@ -21,35 +21,35 @@ resource "azurerm_resource_group" "rg" {
   # }
 }
 
-resource "azurerm_application_insights" "appinsights" {
-  application_type    = "web"
-  location            = var.location
-  name                = "ai-${var.disambiguation}-${random_string.suffix.result}"
-  resource_group_name = azurerm_resource_group.rg.name
-  sampling_percentage = 0
-  depends_on = [
-    azurerm_resource_group.rg
-  ]
-}
-
-
-# Create the Linux App Service Plan
-resource "azurerm_service_plan" "appserviceplan" {
-  name                = "webapp-asp-${var.disambiguation}-${random_string.suffix.result}"
+resource "azurerm_log_analytics_workspace" "workspace" {
+  name                = "la-${var.disambiguation}-${random_string.suffix.result}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Linux"
-  sku_name            = "B1"
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
 }
 
-# Create the web app, pass in the App Service Plan ID
-resource "azurerm_linux_web_app" "webapp" {
-  name                  = "webapp-${var.disambiguation}-${random_string.suffix.result}"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  service_plan_id       = azurerm_service_plan.appserviceplan.id
-  https_only            = true
-  site_config {
-    minimum_tls_version = "1.2"
+resource "azapi_resource" "managed_environment" {
+  name      = "me-${var.disambiguation}-${random_string.suffix.result}"
+  location  = var.location
+  parent_id = azurerm_resource_group.rg.id
+  type      = "Microsoft.App/managedEnvironments@2022-03-01"
+
+  body = jsonencode({
+    properties = {
+      appLogsConfiguration = {
+        destination = "log-analytics"
+        logAnalyticsConfiguration = {
+          customerId = azurerm_log_analytics_workspace.workspace.id
+          sharedKey  = azurerm_log_analytics_workspace.workspace.primary_shared_key
+        }
+      }
+    }
+  })
+
+  lifecycle {
+    ignore_changes = [
+        tags
+    ]
   }
 }
